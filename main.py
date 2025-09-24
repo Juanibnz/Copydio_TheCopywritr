023f14c58ab8b2a966d808aa98de57314eca8611
Juan.Ibanez <juan.ibanez515@gmail.com>
2025-09-24 14:26:59 -0500

    Updated content

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import lib.utils as utils
 
 st.title("Copydio - Generador de Copys para Redes Sociales 🧑‍💻")
 st.divider()
-st.write("Genera copys, captions y hashtags para posts de redes sociales usando IA.")
+st.write("Genera copys, captions y hashtags para posts de redes sociales usando IA. Este agente está optimizado para Startup Grind Bogotá. \n Si te gustaría tener esta solución en tu negocio, contáctame 😉.")
 st.write("Hecho por [Juan Ibáñez](https://www.linkedin.com/in/juan-ibanez-patino/).")
 st.divider()
 
 with st.form("Infórmale a Copydio 🫡"):
     redesSociales = st.text_input("### Paso 1: ¿En que redes sociales va a estar este copy?")
     intencionComunica = st.text_input("### Paso 2: ¿Qué quieres comunicar?")
-    api_key = st.text_input("### Ingresa tu API Key de Google Gemini (necesario para ejecutar el modelo)", type="password")
+    api_key = st.text_input("### Paso 3: Ingresa tu API Key de Google Gemini (necesario para ejecutar el modelo)", type="password")
     st.markdown("Si no tienes una API Key, sigue los pasos en este [link](https://developers.generativeai.google/tutorials/get-api-key) para obtener una.")
-    files = st.file_uploader("### Paso 3: Sube el contenido base para generar el copy")
+    files = st.file_uploader("### Paso 4: Sube el contenido base para generar el copy")
 
     submitted = st.form_submit_button("Generar copys 🚀")
 
